f6f696b02a41d0b0a46af08cbb851ca65115c349
Dudu <>
2025-04-17 15:22:23 -0300

    salvos

--- exe010.py
+++
@@ -1,8 +0,0 @@
-# Crie um programa que leia quantos dinheiro uma pessoa tem na carteira e mostre quantos Dólares ela pode comprar.
-# considere US$1,00 = R$5.68
-
-real = float(input('Quantos dinheiro você tem na carteira?: R$'))
-dolar = real / 5.68
-euro = real / 6.13
-libra = real / 7.34
-print('Com R${:.2f} você pode comprar US${:.2f} EUR${:.2f} GBP${:.2f}'.format(real, dolar, euro, libra))
--- exe014.py
+++
@@ -1,5 +0,0 @@
-# Escreva um programa que converta uma temperatura digitada em ºC a converta para ºF
-
-c = float(input('Informe a temperatura em ºC:'))
-f = (c * 1.8) + 32
-print('A temperatura de {}ºC correspode a {}ºF!'.format(c , f))
